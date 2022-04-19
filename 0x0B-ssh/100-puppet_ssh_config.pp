aae44c97052f9ee24f51acf201ffbe7427517332
Hugo Machacon <hmachacom@gmail.com>
2022-04-19 17:33:41 -0500

    add 100

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+file_line { 'ssh_config':
+    ensure   => present,
+    path     => '/etc/ssh/ssh_config',
+    line     => 'PasswordAuthentication no',
+    multiple => 'true'
+}
+
+file_line { 'ssh_config_2':
+    ensure   => present,
+    path     => '/etc/ssh/ssh_config',
+    line     => 'IdentityFile ~/.ssh/school',
+    multiple => 'true'
+}
