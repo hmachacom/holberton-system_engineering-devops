d220b57dba10e219912c601c8ee446574fe1d572
Hugo Machacon <hmachacom@gmail.com>
2022-05-19 11:58:53 -0500

    style

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,9 +10,9 @@ if __name__ == "__main__":
     ) as nameEmploy:
 
         for i in json.load(nameEmploy):
-            if i["id"] == int(argv[1]):
-                name = i["name"]
-                id = i["id"]
+            if i.get("id") == int(argv[1]):
+                name = i.get("name")
+                id = i.get("id")
         with urllib.request.urlopen(
             f"https://jsonplaceholder.typicode.com/users/{id}/todos"
         ) as taskEmploy:
@@ -20,9 +20,9 @@ if __name__ == "__main__":
             tasksTotal = 0
             nameTaskDone = []
             for task in json.load(taskEmploy):
-                if task["completed"]:
+                if task.get("completed"):
                     tasksDone += 1
-                    nameTaskDone.append(task["title"])
+                    nameTaskDone.append(task.get("title"))
                 tasksTotal += 1
         print(f"Employee {name} is done with tasks({tasksDone}/{tasksTotal}):")
         print("\t ", end='')
