c413b2239d5860b16688fdbd7f818e104c2f7844
Hugo Machacon <hmachacom@gmail.com>
2022-05-19 12:09:47 -0500

    add  get

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ Gather data from an API """
+from sys import argv
 import json
 import urllib.request
-from sys import argv
 
 if __name__ == "__main__":
     with urllib.request.urlopen(
