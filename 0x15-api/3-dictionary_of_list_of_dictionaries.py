5ddebc52c1174f5b5f4c3919ac32f2a1b06f9a68
Hugo Machacon <hmachacom@gmail.com>
2022-05-19 14:43:11 -0500

    3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" Gather data from an API and Export to CSV"""
+""" Using what you did in the task #0, extend your
+Python script to export data in the JSON format."""
 import requests
 import sys
 import json
