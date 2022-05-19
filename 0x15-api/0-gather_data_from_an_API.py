fc1c4927ac9b5eb27463c9b9f4e2a7eac073e407
Hugo Machacon <hmachacom@gmail.com>
2022-05-19 12:28:54 -0500

    update requests

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,27 +1,24 @@
 #!/usr/bin/python3
 """ Gather data from an API """
-import json
 from sys import argv
-import urllib.request
+import requests
 
 if __name__ == "__main__":
-    with urllib.request.urlopen(
+    name = requests.get(
         f"https://jsonplaceholder.typicode.com/users/{argv[1]}"
-    ) as nameEmploy:
-        date = json.load(nameEmploy)
-        name = date.get("name")
-        id = date.get("id")
-        with urllib.request.urlopen(
-            f"https://jsonplaceholder.typicode.com/users/{id}/todos"
-        ) as taskEmploy:
-            tasksDone = 0
-            tasksTotal = 0
-            nameTaskDone = []
-            for task in json.load(taskEmploy):
-                if task.get("completed"):
-                    tasksDone += 1
-                    nameTaskDone.append(task.get("title"))
-                tasksTotal += 1
-        print(f"Employee {name} is done with tasks({tasksDone}/{tasksTotal}):")
-        print("\t ", end="")
-        print("\n\t ".join(nameTaskDone))
+    ).json().get('name')
+
+    taskEmploy = requests.get(
+            f"https://jsonplaceholder.typicode.com/users/{argv[1]}/todos"
+        ).json()
+    tasksDone = 0
+    tasksTotal = 0
+    nameTaskDone = []
+    for task in taskEmploy:
+        if task.get("completed"):
+            tasksDone += 1
+            nameTaskDone.append(task.get("title"))
+        tasksTotal += 1
+    print(f"Employee {name} is done with tasks({tasksDone}/{tasksTotal}):")
+    print("\t ", end="")
+    print("\n\t ".join(nameTaskDone))
