03151af3deb4dfd9927b96a636f20a0e2397cac3
Hugo Machacon <hmachacom@gmail.com>
2022-05-19 15:08:11 -0500

    import

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ Using what you did in the task #0, extend your
 Python script to export data in the JSON format."""
-import requests
-import sys
-import json
 
 if __name__ == "__main__":
+    import requests
+    import sys
+    import json
 
     users = (requests.get("https://jsonplaceholder.typicode.com/users")).json()
 
