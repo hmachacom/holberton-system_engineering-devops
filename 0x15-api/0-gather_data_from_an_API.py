ce167ea22b64dc9839ac0cf0bba98f51733016f0
Hugo Machacon <hmachacom@gmail.com>
2022-05-19 12:51:19 -0500

    pyc

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,7 +7,8 @@ if __name__ == "__main__":
 
     argv = sys.argv[1]
     name = (
-        requests.get("https://jsonplaceholder.typicode.com/users/{}".format(argv))
+        requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                     .format(argv))
         .json()
         .get("name")
     )
@@ -22,7 +23,9 @@ if __name__ == "__main__":
         if task.get("completed") and task.get('userId') == int(argv):
             tasksDone += 1
             nameTaskDone.append(task.get("title"))
-        tasksTotal += 1
-    print("Employee {} is done with tasks({}/{}):".format(name, tasksDone,tasksTotal))
+        if task.get('userId') == int(argv):
+            tasksTotal += 1
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, tasksDone, tasksTotal))
     print("\t ", end="")
     print("\n\t ".join(nameTaskDone))
